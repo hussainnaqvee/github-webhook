38bbbcc8d3919bd10d82d7d0999201dd9895edfb
Hussain Ali <hussain.naqvee@safarifoneict.com>
2024-05-06 15:59:38 +0500

    refactored the git pull logic

--- index.js
+++ index.js
@@ -3,33 +3,43 @@ const https = require('https');
 const fs = require('fs');
 const { exec } = require('child_process');
 const crypto = require('crypto');
-require('dotenv').config();
+require("dotenv").config();
 
-const hmac = crypto.createHmac('sha1', process.env.GITHUB_SECRET);
+const hmac = crypto.createHmac("sha1", process.env.GITHUB_SECRET);
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
+
+//config variables for github
+let isDocumentationWebsiteUpdated = false;
+let isContributorsUpdated = false;
+let isMindmapUpdated = false;
+
+let documentationWebsiteBuildTime = Date.now();
+let mindmapBuildTime = Date.now();
+let contributorsBuildTime = Date.now();
 
 app.use(express.json());
 
-app.post('/webhook', async(req, res) => {
-    console.log('req receieved');
-    const signature = req.headers['x-hub-signature'];
-    const payload = JSON.stringify(req.body);
+app.post("/webhook", async (req, res) => {
+  console.log("req receieved");
+  const signature = req.headers["x-hub-signature"];
+  const payload = JSON.stringify(req.body);
 
-    const calculatedSignature = `sha1=${hmac.update(payload).digest('hex')}`;
+  const calculatedSignature = `sha1=${hmac.update(payload).digest("hex")}`;
 
-    if (crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(calculatedSignature))) {
-        await getBranchStatus(req);
-        res.sendStatus(200);
-    } else {
-        console.log('Invalid signature');
-        res.sendStatus(400);
-    }
+  if (crypto.timingSafeEqual(Buffer.from(signature), Buffer.from(calculatedSignature))) {
+    const result = await getBranchStatus(req);
+    result ? res.sendStatus(result) : res.sendStatus(400);
+    // res.sendStatus(200);
+  } else {
+    console.log("Invalid signature");
+    res.sendStatus(400);
+  }
 });
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on port ${port}`);
 });
 
 /*
@@ -43,49 +53,75 @@ https.createServer({
 //https://smee.io/fk2ncMRBOxWycBQ
 
 const getBranchStatus = async (req) => {
-    console.clear();
-    // console.log(req.body);
-    console.log('Webhook received successfully');
+  console.clear();
+  // console.log(req.body);
+  console.log("Webhook received successfully");
 
-    var branchNames = req.body['ref'].split('/');
-    var branchName = branchNames.pop();
-    var headCommit = req.body['head_commit'];
-    var addedFiles = headCommit['added'];
-    var removedFiles = headCommit['removed'];
-    var modifiedFiles = headCommit['modified'];
+  var branchNames = req.body["ref"].split("/");
+  var branchName = branchNames.pop();
+  var headCommit = req.body["head_commit"];
+  var addedFiles = headCommit["added"];
+  var removedFiles = headCommit["removed"];
+  var modifiedFiles = headCommit["modified"];
 
-    console.log(`Branch Name: ${branchName}`);
-        if (branchName === process.env.BRANCH_NAME) {
-        
-        if (addedFiles.length > 0) {
-            console.log(`Added Files: ${addedFiles}`);
-        }
-        if (removedFiles.length > 0) {
-            console.log(`Removed Files: ${removedFiles}`);
-        }
-        if (modifiedFiles.length > 0) {
-            console.log(`Modified Files: ${modifiedFiles}`);
-            let isModified = modifiedFiles.some((str)=>str.includes("src"));
-            if(isModified){
-                console.log("Modified src files");
-                
-                await executeCmd(process.env.EXEC_CMD);
-            }
-        }
-}
-}
+  console.log(`Branch Name: ${branchName}`);
+  return branchName === process.env.BRANCH_NAME ? await buildProject(branchName, addedFiles, removedFiles, modifiedFiles) : null;
+};
 
-const executeCmd = (cmd) => {
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                reject(error);
-            } else {
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-                resolve(stdout);
-            }
-        });
+const executeCmd = async (cmd) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(stderr + "\n" + stdout);
+      } else {
+        resolve(stderr + "\n" + stdout);
+      }
     });
+  });
+};
+
+const isProjectUpdated = (projectName,addedFiles,modifiedFiles,removedFiles) => { 
+    return addedFiles.some((str) => str.includes(projectName)) || modifiedFiles.some((str) => str.includes(projectName)) || removedFiles.some((str) => str.includes(projectName));
+}
+
+const buildProject = async (branchName, addedFiles, removedFiles, modifiedFiles) => { 
+    if (branchName === process.env.BRANCH_NAME) {
+      if (isProjectUpdated("mindmap", addedFiles, modifiedFiles, removedFiles)) {
+        isMindmapUpdated = true;
+      }
+      if (isProjectUpdated("documentation-website", addedFiles, modifiedFiles, removedFiles)) {
+        isDocumentationWebsiteUpdated = true;
+      }
+      if (isProjectUpdated("contributors", addedFiles, modifiedFiles, removedFiles)) {
+        isContributorsUpdated = true;
+      }
+    }
+    //pull the project
+    executeCmd(`${process.env.PROJECT_PATH} && ${process.env.PULL_CMD}`);
+    
+    //make sure npm install all libraries
+    executeCmd(`${process.env.PROJECT_PATH} && npm install`);
+    //build the mindmap
+    if (isMindmapUpdated || true) {
+      console.log("Building Mindmap");
+      executeCmd(`${process.env.PROJECT_PATH}/mindmap && npm run build`);
+      mindmapBuildTime = Date.now();
+      isMindmapUpdated = false;
+    }
+    //build the contributors
+    if (isContributorsUpdated || true) {
+      console.log("Building Contributors");
+      executeCmd(`${process.env.PROJECT_PATH}/documentation-website && npm run contributors`);
+      contributorsBuildTime = Date.now();
+      isContributorsUpdated = false;
+    }
+    //build the documentation website
+    if (isDocumentationWebsiteUpdated || true) {
+      console.log("Building Documentation Website");
+      executeCmd(`${process.env.PROJECT_PATH}/documentation-website && npm run build`);
+      documentationWebsiteBuildTime = Date.now();
+      isDocumentationWebsiteUpdated = false;
+    }
+    return 200;
 }
--- projectBuilder.js
+++ projectBuilder.js
@@ -1,31 +1,64 @@
 const { exec } = require('child_process');
 require('dotenv').config();
 
-const executeCmd = (cmd) => {
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`exec error: ${error}`);
-                reject(error);
-            } else {
-                console.log(`stdout: ${stdout}`);
-                console.error(`stderr: ${stderr}`);
-                resolve(stdout);
-            }
-        });
+const executeCmd = async (cmd) => {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        reject(stderr + "\n" + stdout);
+      } else {
+        resolve(stderr + "\n" + stdout);
+      }
     });
-}
+  });
+};
 
 //git pull the project
-executeCmd(`${process.env.PROJECT_PATH} & ${process.env.PULL_CMD}`);
+// executeCmd(`${process.env.PROJECT_PATH} & ${process.env.PULL_CMD}`);
+
+//make sure npm install all libraries
+executeCmd(`${process.env.PROJECT_PATH} && npm install`)
+  .then((output) => {
+    console.log(output);
+  })
+  .catch((error) => {
+    {
+      console.error(error);
+    }
+  });
 
 //git build mindmap
-// executeCmd(`${process.env.PROJECT_PATH}\\documentation-website & npm run contributors`);
+executeCmd(`${process.env.PROJECT_PATH}/mindmap && npm install && npm run build`)
+  .then((output) => {
+    console.log(output);
+  })
+  .catch((error) => {
+    {
+      console.error(error);
+    }
+  });
 
 //git run contributer-build
-executeCmd(`${process.env.PROJECT_PATH}/documentation-website & npm run build`);
+executeCmd(`${process.env.PROJECT_PATH}/documentation-website && npm install && npm run contributors`)
+  .then((output) => {
+    console.log(output);
+  })
+  .catch((error) => {
+    {
+      console.error(error);
+    }
+  });
 
 //git build documentation website
-executeCmd(`${process.env.PROJECT_PATH}/documentation-website & npm run dev`)
+executeCmd(`${process.env.PROJECT_PATH}/documentation-website && npm run build`)
+  .then((output) => {
+    console.log(output);
+  })
+  .catch((error) => {
+    {
+      console.error(error);
+    }
+  });
 
-
+module.exports = executeCmd;
